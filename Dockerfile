2325d4e07bce05e3a60105c02c4e5ad44520dab5
Andrew Macheret <andrew.macheret@sap.com>
2017-02-02 11:26:56 -0800

    Switch from stockfish glibc to stockfish npm

--- Dockerfile
+++ Dockerfile
@@ -1,32 +1,6 @@
 FROM mhart/alpine-node 
 
-# download and install glibc
-ENV GLIBC_VERSION 2.23-r3
-RUN apk add --update curl && \
-  curl -Lo /etc/apk/keys/sgerrand.rsa.pub https://raw.githubusercontent.com/sgerrand/alpine-pkg-glibc/master/sgerrand.rsa.pub && \
-  curl -Lo glibc.apk "https://github.com/sgerrand/alpine-pkg-glibc/releases/download/${GLIBC_VERSION}/glibc-${GLIBC_VERSION}.apk" && \
-  apk add glibc.apk && \
-  curl -Lo glibc-bin.apk "https://github.com/sgerrand/alpine-pkg-glibc/releases/download/${GLIBC_VERSION}/glibc-bin-${GLIBC_VERSION}.apk" && \
-  apk add glibc-bin.apk && \
-  /usr/glibc-compat/sbin/ldconfig /lib /usr/glibc-compat/lib && \
-  echo 'hosts: files mdns4_minimal [NOTFOUND=return] dns mdns4' >> /etc/nsswitch.conf &&\
-  apk del curl &&\
-  rm -rf glibc.apk glibc-bin.apk /var/cache/apk/*
-
-# install node, bash, expect
-RUN apk add --update bash expect &&\
-  rm -rf glibc.apk glibc-bin.apk /var/cache/apk/*
-
 WORKDIR /app
-
-# download stockfish
-RUN apk add --update openssl &&\
-  wget 'https://stockfish.s3.amazonaws.com/stockfish-6-linux.zip' &&\
-  unzip stockfish-6-linux.zip &&\
-  rm stockfish-6-linux.zip &&\
-  rm -r __MACOSX/ &&\
-  apk del openssl &&\
-  rm -rf glibc.apk glibc-bin.apk /var/cache/apk/*
 
 ADD app .
 
--- app/chess-server.js
+++ app/chess-server.js
@@ -3,34 +3,36 @@ var compression = require('compression');
 //var apicache = require('apicache').options({ debug: true }).middleware;
 //var bodyParser = require('body-parser');
 var app = express();
-var spawn = require('child_process').spawn;
 var path = require('path');
 var urlencode = require('urlencode');
 //app.use(bodyParser.urlencoded({extended: true}));
 
+var stockfish = require("stockfish");
+
 var fs = require('fs');
 var settings = JSON.parse(fs.readFileSync(path.resolve(__dirname, './settings.json'), 'utf8'));
 
-var move_binary = path.resolve(__dirname, './move.sh');
+function getBestMove(options, callback) {
+  var instance = stockfish();
 
-function run(command, args, callback) {
-  console.log(command, args);
-  var child = spawn(command, args);
-  var response = '';
-  var error = '';
-  child.stdout.on('data', function(buffer) {
-    response += buffer.toString();
-  });
-  child.stderr.on('data', function(buffer) {
-    error += buffer.toString();
-  });
-  child.on('close', function(code) {
-    if (code === 0) {
-      callback(response);
-    } else {
-      callback(null, error);
+  // set the callback for each message
+  instance.onmessage = function(event) {
+    var message = event.data || event;
+    
+    console.log(typeof event, event);
+    
+    if (typeof message === 'string') {
+      //'bestmove g1f3 ponder g8f6'
+      var split = message.split(/ +/);
+      if (split[0] == 'bestmove') {
+        callback(split[1]);
+      }
     }
-  });
+  };
+
+  // set the fen, then perform the search with the given depth
+  instance.postMessage('position fen ' + options.fen);
+  instance.postMessage('go depth ' + options.depth);
 }
 
 //app.use(compression());
@@ -44,7 +46,7 @@ app.use(compression({
 console.log('registering /');
 app.get('/' /*, apicache('5 minutes')*/, function(req, res) {
   console.log('GET ' + req.originalUrl);
-  res.setHeader('Access-Control-Allow-Origin', 'https://andrewmacheret.com');
+  //res.setHeader('Access-Control-Allow-Origin', 'https://andrewmacheret.com');
 
   res.set({
     'Content-Type': 'application/json'
@@ -57,7 +59,7 @@ app.get('/' /*, apicache('5 minutes')*/, function(req, res) {
 console.log('registering /moves');
 app.get('/moves' /*, apicache('5 minutes')*/, function(req, res) {
   console.log('GET ' + req.originalUrl);
-  res.setHeader('Access-Control-Allow-Origin', 'https://andrewmacheret.com');
+  //res.setHeader('Access-Control-Allow-Origin', 'https://andrewmacheret.com');
 
   res.set({
     'Content-Type': 'application/json'
@@ -76,7 +78,7 @@ app.get('/moves' /*, apicache('5 minutes')*/, function(req, res) {
   var depth = parseInt(req.query.depth, 10) || 15;
   depth = Math.min(depth, 20);
 
-  run(move_binary, [fen, depth.toString()], function(moveResponse, err) {
+  getBestMove({fen: fen, depth: depth}, function(moveResponse, err) {
     if (err) {
       console.error(err);
       res.status(500);
